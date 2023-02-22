7cf3406951210176d639935b412c2a47166a829e
Pavan <pavanchambhare88@gmail.com>
2023-02-22 20:54:56 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { useEffect } from "react";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Newsletter from "./components/Newsletter";
+import Portfolio from "./components/Portfolio";
+import Products from "./components/Products";
+import ScrollToTop from "./components/ScrollToTop";
+import Services from "./components/Services";
+import Testimonials from "./components/Testimonials";
+
+export default function App() {
+  return (
+    <>
+      <ScrollToTop />
+      <Navbar />
+      <Hero />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Products />
+      <Newsletter />
+      <Footer />
+    </>
+  );
+}
---
+++ src/components/scrollreveal.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const scrollreveal = (props) => {
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  )
+}
+
+export default scrollreveal
